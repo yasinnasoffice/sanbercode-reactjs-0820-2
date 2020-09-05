78ebf016ba5efe68bc5d945ce7d0c79257ae1630
Muhammad Yasin Nasrulloh <70148208+yasinnasoffice@users.noreply.github.com>
2020-09-05 07:26:39 +0700

    merubah component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,48 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import './TUGAS-10/tugas10.css';
+import './TUGAS-9/tugas9.js'
+import './TUGAS-10/tugas10.js'
+//import Tampil from './TUGAS-9/tugas9.js';
+//import  AnekaBuah from './TUGAS-10/tugas10.js';
+
 
 function App() {
   return (
-    <div className="App">
-      <table class="table" align="center">
-        <thead>
-          <tr>
-            <th colSpan="2" text-align="center">Form Pembelian Buah</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Nama Pelanggan :</td>
-            <td align="left"><input type="text" width="10"></input></td>
-          </tr>
-          <tr>
-            <td align="left">Daftar Item</td>
-            <td align="left">
-              <input type="checkbox"></input>
-              <label>Semangka</label><br/>
-              <input type="checkbox"></input>
-              <label>Jeruk</label><br/>
-              <input type="checkbox"></input>
-              <label>Nanas</label><br/>
-              <input type="checkbox"></input>
-              <label>Salak</label><br/>
-              <input type="checkbox"></input>
-              <label>Anggur</label>
-            </td>
-          </tr>
-          <tr>
-          <th colSpan="2" align="left"> <input type="submit" value="Kirim"></input></th>
-          </tr>
-
-        </tbody>
-      </table>
+    <div>
+    <tugas9 /> 
+    <tugas10 /> 
     </div>
   );
+ 
 }
 
 
 
 export default App;
+
+//export default Tampil;
+
+
+
 
 
